793d4e1e9f3edc86c9fa0998e850d2a4ce693ce7
Nova Wolfcreek <novawko@gmail.com>
2025-08-28 07:57:43 -0500

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -13,15 +13,10 @@ document.addEventListener("DOMContentLoaded", async () => {
   /* Designs
   ===================================================================== */
   let designs = await charadex.initialize.page(null, charadex.page.index.designs, (arr) => {
-  // Force sliceAmount to 8
-  let sliceAmount = 8;
 
-  // Get the last 8 (or fewer if less than 8 exist)
-  let recent = arr.slice(-sliceAmount);
-
-  // Overwrite original array in-place
-  arr.length = 0;
-  arr.push(...recent);
+  // Splice the silly little array
+  let sliceAmount = charadex.page.index.designs.amount || 6;
+  arr.splice(sliceAmount, arr.length);
 
   // Reverse the thing
   let designs = arr.reverse();
